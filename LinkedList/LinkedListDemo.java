cfca734d4257f669afd87f162a76b3961bb9dc6b
Brahmendra Sravan Kumar Patibandla <pbskumar@live.com>
2016-05-25 19:06:14 -0400

    rotation, sum

--- LinkedList/LinkedListDemo.java
+++ LinkedList/LinkedListDemo.java
@@ -296,6 +296,36 @@ class LinkedList {
 		
 		return tempHead;
 	}
+	
+	public LinkedList rotateLinkedList(int k) {
+		// Given a number k, the linked list will be rotated right by k nodes
+		// 4->5->6->7->8 rotated by k=2 becomes 7->8->4->5->6
+		
+		int i = 0;
+		Node rotateHead = head;
+		Node temp = head;
+		
+		while (i < k && temp != null) {
+			i++;
+			temp = temp.next;
+		}
+		
+		if (temp == null)
+			return new LinkedList();
+		
+		while (temp.next != null) {
+			temp = temp.next;
+			rotateHead = rotateHead.next;
+		}
+		
+		temp.next = head;
+		head = rotateHead.next;
+		rotateHead.next = null;
+		
+		return new LinkedList(head);
+		
+	}
+	
 }
 
 
@@ -308,8 +338,6 @@ public class LinkedListDemo {
 		Node current1 = list1.getHead();
 		Node current2 = list2.getHead();
 		
-		System.out.println(current1.data);
-		System.out.println(current2.data);
 		
 		while (true) {
 			if(current1 == null && current2 == null)
@@ -346,9 +374,84 @@ public class LinkedListDemo {
 		
 	}
 	
+	public static LinkedList mergeSort(Node head) {
+		if(head.next == null)
+			return new LinkedList(head);
+		
+		// Get central node in the list
+		// Node centralNode = getCenterNode(head);
+		
+		Node first = head;
+		//Node second = head.next;
+		//head.next = null;
+		Node second = getCenterNode(head);
+		
+		LinkedList sortedSubList1 = mergeSort(first);
+		LinkedList sortedSubList2 = mergeSort(second);
+		
+		
+		Node sortedList = mergeSortedLinkedLists(sortedSubList1, sortedSubList2).getHead();
+		
+		return new LinkedList(sortedList);
+	}
+	
+	public static Node getCenterNode(Node head) {
+		Node fastRef = head;
+		if (fastRef == null || fastRef.next == null) 
+			return null;
+		
+		Node slowRef = fastRef;
+		while (fastRef != null) {
+			fastRef = fastRef.next;
+			if(fastRef != null && fastRef.next != null) {
+				fastRef = fastRef.next;
+				slowRef = slowRef.next;
+			}
+		}
+		
+		Node centerRef = slowRef.next;
+		slowRef.next = null;
+		return centerRef;
+		
+	}
+	
+	public static LinkedList sumOfLL(LinkedList list1, LinkedList list2) {
+		// Returns sum of numbers which are represented as linked lists
+		// 234 is represented as 4->3->2
+		
+		Node temp1 = list1.getHead();
+		Node temp2 = list2.getHead();
+		
+		int a = 0, b =0, sum = 0, carry = 0;
+		
+		LinkedList sumList = new LinkedList();
+		
+		while (temp1 != null || temp2 != null) {
+			a = (temp1 != null)? temp1.data: 0;
+			b = (temp2 != null)? temp2.data: 0;
+			
+			sum = a + b + carry;
+			carry = sum / 10;
+			sum = sum % 10;
+			
+			sumList.appendNode(sum);
+			
+			if(temp1 != null)
+				temp1 = temp1.next;
+			
+			if(temp2 != null)
+				temp2 = temp2.next;
+			
+		}
+		
+		if(carry > 0) sumList.appendNode(carry);
+		
+		return sumList;
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
 			
-		/*	
+		
 		Scanner sc = new Scanner(System.in);
 		LinkedList lList = new LinkedList();
 		
@@ -361,12 +464,59 @@ public class LinkedListDemo {
 		System.out.println("\n\n");
 		System.out.println("The Linked List created is:");
 		lList.printLinkedList();
-		*/
+		
+		System.out.println("\n\n");
+		System.out.println("The rotated Linked List created is:");
+		lList.rotateLinkedList(2).printLinkedList();
+		
+		
+		/*
+		// Unit test for sum of linked lists
 		
 		LinkedList lList1 = new LinkedList();
 		LinkedList lList2 = new LinkedList();
 		
-		File file1 =  new File("input.txt");
+		File file1 =  new File("input3.txt");
+		File file2 =  new File("input4.txt");
+		
+		Scanner fsc1 = new Scanner(file1);	
+		Scanner fsc2 = new Scanner(file2);	
+		
+		
+		while (fsc1.hasNext()) 
+			lList1.appendNode(fsc1.nextInt());
+		
+		while (fsc2.hasNext()) 
+			lList2.appendNode(fsc2.nextInt());
+	
+		
+		System.out.println("\n");
+		System.out.println("Linked List 1:");
+		lList1.printLinkedList();
+		
+		System.out.println("\n");
+		System.out.println("Linked List 2:");
+		lList2.printLinkedList();
+		
+		System.out.println("\n\n");
+		System.out.println("Sum of the Lists:");
+		sumOfLL(lList1, lList2).printLinkedList();
+		*/
+		
+		
+		/*
+		// Test unit for mergeSort
+		System.out.println("\n\n");
+		System.out.println("The sorted Linked List is:");
+		mergeSort(lList.getHead()).printLinkedList();
+		*/
+		
+		
+		/*
+		LinkedList lList1 = new LinkedList();
+		LinkedList lList2 = new LinkedList();
+		
+		File file1 =  new File("input1.txt");
 		File file2 =  new File("input2.txt");
 		
 		Scanner fsc1 = new Scanner(file1);	
@@ -393,7 +543,7 @@ public class LinkedListDemo {
 		mergeSortedLinkedLists(lList1, lList2).printLinkedList();
 		
 		
-		
+		*/
 		
 		/*
 		System.out.println("\n\n");
